35d1d7509605abfc5e7a2579a86d9b3855252638
Luis Olivar <lao294@nyu.edu>
2018-06-21 22:30:31 -0400

    Added route file

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
