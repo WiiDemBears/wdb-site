1e40078530f2cb71244d664b5923a3fca8f23ee8
Ahmad Raza <raza2157@gmail.com>
2018-06-23 01:51:40 -0400

    Reformatted code

--- app/routes.js
+++ app/routes.js
@@ -1,48 +1,39 @@
-const mongoose = require('mongoose');
-const User = require('../app/models/user');
+const mongoose = require("mongoose");
+const User = require("../app/models/user");
 
-module.exports = function(app, passport){
+module.exports = function(app, passport) {
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
 
-    app.get('/', (req, res) => {
-        res.render('index');
-    });
+  app.get("/forgot_password", (req, res) => {
+    res.render("forgot_password");
+  });
 
-<<<<<<< HEAD
-    app.get('/forgot_password', (req, res) => {
-        res.render('forgot_password');
-    });
+  app.get("/memes", (req, res) => {
+    res.render("memes");
+  });
 
-    app.get('/memes', (req, res) => {
-        res.render('memes');
-    });
+  app.get("/quotes", (req, res) => {
+    res.render("quotes");
+  });
 
-    app.get('/quotes', (req, res) => {
-        res.render('quotes');
-    });
+  app.get("/down", (req, res) => {
+    res.render("down");
+  });
 
-    app.get('/down', (req, res) => {
-        res.render('down');
-    });
+  app.get("/register", (req, res) => {
+    res.render("register");
+  });
 
-    app.get('/register', (req, res) => {
-        res.render('register');
-    });
-
-=======
->>>>>>> baab64929582c0f8a033016066c8dc36657e85f3
-    /*
+  /*
         Middleware functions are called between requests... 
     */
-    function isLoggedIn(req, res, next){
-
-        if(req.isAuthenticated()){
-            return next();
-        }
-
-        res.redirect('/');
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
 
-
-
-
+    res.redirect("/");
+  }
 };
