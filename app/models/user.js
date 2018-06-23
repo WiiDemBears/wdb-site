769f37aacb8019acb9c54d2ca1245f003339c8a0
Luis Olivar <lao294@nyu.edu>
2018-06-23 00:14:01 -0400

    Added a user schema file, updated directory structure

---
+++ app/models/user.js
@@ -0,0 +1,37 @@
+/*
+    User model for Database. Look into using async functions for bcrypt as they are less taxing on DB server.
+ */
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const URLSlugs = require('mongoose-url-slugs');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+    local:{
+        username: {
+            type: String,
+            unique: true
+        },
+        password: String,
+        profile: {type:Schema.Types.ObjectId, ref: 'Profile'},
+    },
+
+});
+
+
+userSchema.plugin(URLSlugs('local.username'));
+
+userSchema.methods.generateHash = function (password){
+
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password){
+
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
