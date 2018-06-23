65683bc32d4b25fef84c24c23375c497a882eaba
Luis Olivar <lao294@nyu.edu>
2018-06-23 00:01:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,45 @@
+/*
+    App.js, or server.js contains the entry point to the web app.
+ */
+
+/* Dependencies */
+
+//Require Express framework for routing pages. This is further explained in /app/routes.js
 const express = require('express');
+
+//Require mongoose, an Object Document Mapper that allows for seamless communication between Node.js and MongoDb
 const mongoose = require('mongoose');
-const passport = require('passport');
+
+//Require express-session. This allows for user sessions.
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
+
+//Require passport. This package allows for authentication of users throughout the app through the use of
+//various authentication strategies. This is further explained in /config/passport.js
+const passport = require('passport');
+
+//Require the path package. This is for naming paths in the directory.
 const path = require('path');
+
+//Set app, our webapp, to a new express object. This initializes a new app. From here, app represents our web-app.
 const app = express();
 
-//connect to database
-require('./config/db.js');
 
-//connect passport configuration
+//connect to passport configuration file, passing in passport package from above.
 require('./config/passport')(passport);
 
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-
-
-app.use(session({
-    secret: 'suchsecretwowe!',
-    resave: true,
-    saveUninitialized: true,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-//enable flash messages for login/register errors
-app.use(flash());
-
-//routes
+//import defined routes, passing in our passport strategy and our defined web-app.
 require('./app/routes.js')(app, passport);
 
+//set our templating engine to Handlebars (hbs).
+app.set('view engine', 'hbs');
 
-//app startup
+//allow for express to parse the body object.
+app.use(express.urlencoded({ extended: false }));
+
+//set a new static path for serving static files. Namely, the public folder.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app startup. If PORT is not defined, use port 3000. Log the port out to the console.
+//Normally, the app is started as defined in the README (node app.js). But Heroku is weird about it...
 app.listen(process.env.PORT || 3000);
-console.log("Serving on port: " + (process.env.PORT || 3000));
+console.log("This is the port: " + (process.env.PORT || 3000));
