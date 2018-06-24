ff2b4210a336ed8ad641383cfadf7a8ed2d40307
Ahmad Raza <raza2157@gmail.com>
2018-06-24 02:37:59 -0400

    Added Material UI support for React components

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,43 @@ const linkStyle = {
 }
 
 const Header = () => (
-    <div>
-        <Link href="/">
-          <a style={linkStyle}>Home</a>
+    <nav>
+      <Link href="/">
+          <a style={linkStyle}>Wii Dem Bears</a>
         </Link>
-        <Link href="/about">
-          <a style={linkStyle}>About</a>
+        <Link href="/down">
+          <a style={linkStyle}>Down</a>
         </Link>
-    </div>
+        <Link href="/quotes">
+          <a style={linkStyle}>Quotes</a>
+        </Link>
+        <Link href="/memes">
+          <a style={linkStyle}>Memes</a>
+        </Link>
+        <style jsx>{`
+      h1, a {
+        font-family: "Roboto";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+    </nav>
 )
 
 export default Header
