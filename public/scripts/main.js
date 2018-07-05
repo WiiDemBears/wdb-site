bea0c0c63610853166bd7633ae1062832763ee7f
Jarman <jarman124@gmail.com>
2018-07-05 12:38:12 -0400

    Changed navbar to responsive for mobile

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,3 +8,12 @@ window.onclick = function(event) {
     modal.style.display = "none"
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
