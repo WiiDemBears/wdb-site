ce2fcf721c16e7dedb30ca21e1ac7c0206c64b2b
Luis Olivar <lao294@nyu.edu>
2018-07-17 22:48:57 -0400

    Added event schema

--- app/models/event.js
+++ app/models/event.js
@@ -6,7 +6,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  eventOwner: { type: Schema.Types.ObjectId, ref: "User" }
+  eventOwner: { type: Schema.Types.ObjectId, ref: "User" },
+  event_date: { type: Date, default: Date.now },
+  event_name: String,
+  event_location: { type: String, default: "No Location Set" },
+  invited: [
+    {
+      invited_user: { type: Schema.Types.ObjectId, ref: "User" },
+      isGoing: { type: Boolean, defualt: false }
+    }
+  ]
 });
 
 module.exports = mongoose.model("Event", eventSchema);
